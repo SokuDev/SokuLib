47aea811d8271826c51d3e4ff84d36cca4579eab
Gegel85 <Gegel85@laposte.net>
2022-05-15 15:44:31 +0200

    Added weather activation routine

--- src/Weather.cpp
+++ src/Weather.cpp
@@ -10,4 +10,5 @@ namespace SokuLib
 	Weather &activeWeather = *reinterpret_cast<Weather *>(ADDR_ACTIVE_WEATHER);
 	Weather &displayedWeather = *reinterpret_cast<Weather *>(ADDR_DISPLAYED_WEATHER);
 	unsigned short &weatherCounter = *reinterpret_cast<unsigned short *>(ADDR_WEATHER_COUNTER);
+	void (__stdcall * const activateWeather)(int weather, char unknown) = reinterpret_cast<void (__stdcall *)(int, char)>(0x439770);
 }
